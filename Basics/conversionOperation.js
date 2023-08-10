a93fee69225fd222be03a02a09f5b6f1e23004db
DnyaneshKalbhut <136080101+DnyaneshKalbhut@users.noreply.github.com>
2023-08-10 17:37:27 +0000

    container config done

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = und;
+
+// console.log(typeof score);
+
+// let valueInnumber = Number(score);
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber)
+
+//"33"=> 33
+//"33abc"=> NaN
+//true=>1 false=>0
+
+let isLoggedIn= "";
+let booleanisLoggedin = Boolean(isLoggedIn)
+//console.log(booleanisLoggedin);
+
+// 1=> true and 0=>false
+//""=>false
+//"dk" =>true
+
+
+let Somenum = 33;
+let stringnumber = String(Somenum);
+console.log(stringnumber);
+console.log(typeof stringnumber);
