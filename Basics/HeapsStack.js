c79f8c5b9a0070f9b74e365d6677d3e5cdb84214
DnyaneshKalbhut <136080101+DnyaneshKalbhut@users.noreply.github.com>
2023-08-12 16:36:09 +0000

    container config done

---
+++ Basics/HeapsStack.js
@@ -0,0 +1,20 @@
+let name1 = "Dnyanesh";
+let name2 = name1;
+name2= "dk"
+
+console.log(name2);
+console.log(name1);
+
+
+
+let employee = {
+    name : "Dnyanesh",
+    salary : 20000
+}
+
+let employee2 = employee;
+
+employee2.salary=10000;
+
+console.log(employee);
+console.log(employee2);
---
+++ Basics/String.js
@@ -0,0 +1,20 @@
+const name = "Dnyanesh";
+const age = 20;
+
+console.log(`hello my name is ${name.toUpperCase()} and my age is ${age}`);
+
+
+const gameName = new String('Dnyanesh-Dk');
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('e'));
+console.log(gameName.length);
+console.log(gameName.replace('Dk','Sk'));
+console.log(gameName.split('-'));
+console.log(gameName.substring(0,4));
+console.log(gameName.trim(' '));
+
+
+
+
+
